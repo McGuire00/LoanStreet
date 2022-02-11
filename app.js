820ba3b669742b0d31a8763d8dab8738c8515b24
Deron McGuire <Deronmcguire23@yahoo.com>
2022-02-10 19:13:08 -0600

    Imported apiRouter and implemented functionality

--- app.js
+++ app.js
@@ -1,8 +1,16 @@
 const express = require("express");
 require("dotenv").config();
 
+const apiRouter = require("./api/api");
+
+// instance of express app
 const app = express();
 
+// json middleware used to parse JSON bodies
+app.use(express.json())
+
+
+app.use("/api", apiRouter)
 
 app.get("/", (req, res) => {
   res.send("Welcome");
